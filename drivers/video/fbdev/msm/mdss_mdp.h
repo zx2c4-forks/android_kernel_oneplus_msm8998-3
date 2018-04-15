2007b1b450bac0572632a198f20133665e465f2c
APOPHIS9283 <steaven.android@gmail.com>
2018-04-15 16:38:26 -0400

    200MS seems more reasonable

--- drivers/video/fbdev/msm/mdss_mdp.h
+++ drivers/video/fbdev/msm/mdss_mdp.h
@@ -58,7 +58,7 @@
 
 /* wait for 300ms to take into account scheduling related delays
  * This number is empirical*/
-#define KOFF_TIMEOUT_MS 300
+#define KOFF_TIMEOUT_MS 200
 #define KOFF_TIMEOUT msecs_to_jiffies(KOFF_TIMEOUT_MS)
 
 #define OVERFETCH_DISABLE_TOP		BIT(0)
