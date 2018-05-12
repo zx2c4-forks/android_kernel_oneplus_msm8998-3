5586a1112f00a92decc0ef863d807c9a5c8ea585
APOPHIS9283 <steaven.android@gmail.com>
2018-05-12 10:59:55 -0400

    Bump boost to 200MS

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -42,7 +42,7 @@ static struct work_struct input_boost_work;
 static unsigned int input_boost_enabled = 1;
 module_param(input_boost_enabled, uint, 0644);
 
-static unsigned int input_boost_ms = 80;
+static unsigned int input_boost_ms = 200;
 module_param(input_boost_ms, uint, 0644);
 
 static bool sched_boost_on_input;
